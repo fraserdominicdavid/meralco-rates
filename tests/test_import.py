dd6cfb4682dfacc851170e25b81c5f386e009cbb
Fraser Dominic David <77269589+fraserdominicdavid@users.noreply.github.com>
2025-05-07 16:16:48 +0800

    build(init): initial commit

---
+++ tests/test_import.py
@@ -0,0 +1,8 @@
+"""Test meralco-rates."""
+
+import meralco-rates
+
+
+def test_import() -> None:
+    """Test that the package can be imported."""
+    assert isinstance(meralco-rates.__name__, str)
